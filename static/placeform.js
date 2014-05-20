298bb44a6852fed2acccae1f7b788b03e70c513c
Mike Cooper <mythmon@gmail.com>
2014-05-19 20:04:12 -0700

    DOM harder.

--- static/placeform.js
+++ static/placeform.js
@@ -1,14 +1,16 @@
-var target = document.querySelector('#form-target');
-var url = target.attributes['data-url'];
+(function() {
+    var target = document.querySelector('#form-target');
+    var url = target.attributes['data-url'].value;
 
-var req = new XMLHttpRequest();
-req.open('GET', url, true);
-req.onreadystatechange = function () {
-    if (req.readyState !== 4) return;
-    if (req.status >= 400) {
-        console.error(req.responseText);
-        return;
-    }
-    target.innerHTML = req.responseText;
-};
-req.send();
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onreadystatechange = function () {
+        if (req.readyState !== 4) return;
+        if (req.status >= 400) {
+            console.error(req.responseText);
+            return;
+        }
+        target.innerHTML = req.responseText;
+    };
+    req.send();
+})();
